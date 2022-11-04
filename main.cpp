9b7472f31a833e1ae00e54b1851e47536504a235
SAAD <Chantal.Hajjar@usj.edu.lb>
2022-11-04 12:50:48 +0200

    lass definition

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class MyVector{
+  public:
+     MyVector(int = 0);
+     MyVector(double*, int size);
+    ~MyVector();
+     MyVector(const MyVector&);  
+     void push_back(double);
+     double pop_back();
+     string toString() const;
+     int getCapacity() const;
+     int getSize() const;
+     MyVector& MyVector::operator=(MyVector v);
+     MyVector& operator+=(const MyVector& v);
+  private:
+    int size;
+    int capacity;
+    double* ptr;  
+};
